c8259bcbbd6b6a81b894067053bfc93baca64353
Laurent Bossavit <laurent.git@bossavit.com>
2018-02-18 16:07:22 +0100

    Parse YAML representation

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,21 @@
 module Main where
 
+import Prelude
+
+import Data.Either
+import Data.List.Types
+import Data.Foreign
+import Data.YAML.Foreign.Decode
+import Control.Monad.Except
+
+data Rules = Rules
+
 data Constant = Constant Number
 
 eval :: Constant -> Number
 eval (Constant num) = num
+
+parseRules :: String -> Either (NonEmptyList ForeignError) Rules
+parseRules text = map makeRules (runExcept $ parseYAMLToJson text)
+
+makeRules _ = Rules
--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -1,12 +1,14 @@
 module Test.Main where
 
-import Main (Constant(..), eval)
+import Main
 
 import Prelude
+import Data.Either
 
 import Test.Unit (suite, test)
 import Test.Unit.Main (runTest)
 import Test.Unit.QuickCheck (quickCheck)
+import Test.Unit.Assert
 
 import Test.QuickCheck ((===))
 
@@ -14,3 +16,15 @@ main = runTest do
   suite "Evaluation" do
     test "Constants eval to themselves" do
       quickCheck $ \ num -> eval (Constant num) === num
+  parsing
+
+rules = """
+- nom: nombre
+  formule: 1
+"""
+
+parsing = suite "Parsing YAML representations" do
+  test "Parse valid representation" do
+    assert "Parse failed" $ isRight (parseRules rules)
+  test "Parse invalid representation" do
+    assert "Parse should fail" $ isLeft (parseRules ": a :")
